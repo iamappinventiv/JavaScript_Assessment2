0ccd0753443f328216ad3a1c9e93bbe26be22971
mac <mac@macs-MacBook-Pro.local>
2023-07-19 16:33:48 +0530

    initial commits

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Assignment is Submited In README.md with Respective Outputs.
+function filterValues(array) {
+    return array.filter((item) => !item ); // filtered Null ,false , 0
+    }
+    var array =  ["Test", true, null, false, 0]
+    console.log(filterValues(array));
