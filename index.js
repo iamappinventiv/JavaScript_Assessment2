d8e17d578edae17c45683a50f5d24a173cd34582
mac <mac@macs-MacBook-Pro.local>
2023-07-19 14:48:13 +0530

    initial commits

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-
-const countArrays = (arr) => {
-  let count = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (Array.isArray(arr[i])) {
-      count++;
-    }
-  }
-  return count;
-};
-
-console.log(countArrays([2, 8, [6], 3, 3, 5, 3, 4, [5, 4]]));
+// Assignment is Submited In README.md
